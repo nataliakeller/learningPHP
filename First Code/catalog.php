dc886ab85e2d2cc0ed1c8739bc58ecbc61b4da0e
nataliakeller <natalia-farah@uergs.edu.br>
2020-07-19 22:51:11 +0200

    learning if else statements and arrays

---
+++ First Code/catalog.php
@@ -0,0 +1,41 @@
+<?php 
+$catalog = array();
+$catalog[101] = "Design Patterns";
+$catalog[201] = "Forrest Gump";
+$catalog[] = "Beethoven";
+$catalog[] = "Clean Code"; // Faz um push no array 
+
+
+$pageTitle = "Full Catalog";
+$section = null;
+
+if (isset($_GET["cat"])) { 
+    if ($_GET["cat"] == "books") {
+        $pageTitle = "Books";
+        $section = "books";
+    } else if ($_GET["cat"] == "movies") {
+        $pageTitle = "Movies";
+        $section = "movies";
+    } else if ($_GET["cat"] == "music") {
+        $pageTitle = "Music";
+        $section = "music";
+    }
+}
+
+include("inc/header.php"); ?>
+
+<div class="section catalog page">
+    <div class="wrapper">
+        <h1><?php echo $pageTitle ?></h1>
+
+        <ul>
+            <?php 
+            foreach($catalog as $item) {
+                echo "<li>" . $item . "</li>";
+            }
+            ?>
+        </ul>
+    </div>
+</div>
+
+<?php include("inc/footer.php"); ?>
---
+++ First Code/suggest.php
@@ -0,0 +1,12 @@
+<?php 
+$pageTitle = "Suggest a Media Item";
+$section = "suggest";
+$section = null;
+
+include("inc/header.php"); ?>
+
+<div class="section page">
+    <h1></h1>
+</div>
+
+<?php include("inc/footer.php"); ?>
